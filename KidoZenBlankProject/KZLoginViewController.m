c9b34a2c9f59fe1135b9c95f2fd899313164823d
Nicolas Miyasato <nmiyasato@gmail.com>
2015-03-04 12:48:55 -0300

    Adding spinner.

--- KidoZenBlankProject/KZLoginViewController.m
+++ KidoZenBlankProject/KZLoginViewController.m
@@ -17,8 +17,11 @@
 #define KidoZenUser @"public@kidozen.com"
 
 @interface KZLoginViewController () <UITextFieldDelegate,KZConnectionManagerDelegate>
+
 @property (nonatomic,strong) KZConnectionManager *kidoZenConector;
 @property (nonatomic,strong) KZResponse *kzResponse;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -35,16 +38,20 @@
 
 - (void)viewDidLoad
 {
+    self.view.userInteractionEnabled = NO;
     [super viewDidLoad];
-    
+    [self.activityIndicator startAnimating];
+
     _kidoZenConector = [KZConnectionManager sharedKZConnectionManager];
     _kidoZenConector.delegate = self;
 
 }
 
 - (void)conectionSuccessfulWithResponse:(KZResponse *)response{
-    
+    self.view.userInteractionEnabled = YES;
     self.kzResponse = response;
+    [self.activityIndicator stopAnimating];
+    self.loginButton.userInteractionEnabled = YES;
 }
 
 - (IBAction)authUser:(id)sender{
@@ -52,13 +59,18 @@
     KZApplication *kzApplication = _kzResponse.application;
     
     __weak KZLoginViewController *safeMe = self;
+    [self.activityIndicator startAnimating];
     
+    // We just login to good technologies.
+    // A webview from Good Technologies will appear, you login using your credentials
+    // and then Kidozen will communicate with Good Technologies to create a kidozen token
     [kzApplication authenticateWithChallenge:@"challenge"
                                     provider:@"Good"
                                   completion:^(id c) {
                                       
+                                      [safeMe.activityIndicator stopAnimating];
+                                      
                                       if (c) {
-                                          
                                           NSString *user= [c description];
                                           if(![user hasPrefix:@"Error"])
                                           {
@@ -80,6 +92,7 @@
                                           {
                                               NSLog(@"The user authentication Failed!");
                                           }
+
                                       }
                                   }];
 }
